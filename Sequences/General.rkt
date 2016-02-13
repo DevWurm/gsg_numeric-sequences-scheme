128451234a10133dab208da0a4f3ee5aad501688
DevWurm <devwurm@gmx.net>
2016-02-13 23:03:48 +0100

    Adding the partial-sum-sequence function to the Sequences/General module. The function creates the partial sum sequence of a provided sequence.

--- Sequences/General.rkt
+++ Sequences/General.rkt
@@ -18,3 +18,17 @@
 (define sum-until
   (lambda (n stm)
     (sum (stream-take n stm))))
+
+(define partial-sum-sequence
+  (lambda (seq)
+    (let
+        ; Creating an infinite list of functions which sum the first n elements of a sequence,
+        ; with n starting by 1.
+        ((sum-functions (stream-map
+                         (lambda (n) (lambda (s) (sum-until n s)))
+                         natural-numbers)))
+      
+      ; Creating the infinite list of results of the sum-functions functions applied to the
+      ; provided sequence. This list is equivalent to the partial sum sequence of the provided
+      ; sequence.
+      (stream-map (lambda (f) (f seq)) sum-functions))))
