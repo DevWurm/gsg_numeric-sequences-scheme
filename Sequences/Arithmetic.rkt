e12707a0a19479ab5af1b94545de2395a4f6354d
DevWurm <devwurm@gmx.net>
2016-02-15 09:28:29 +0100

    Adding Sequences/Arithmetic module, which provides functions to generate explicit and recursive arithmetic sequences.

---
+++ Sequences/Arithmetic.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(require "General.rkt")
+
+(provide rec-ar-sequence)
+;(provide exp-ar-sequence)
+
+(define rec-ar-sequence
+  (lambda (a0 p)
+    (rec-sequence a0 (lambda (n) (+ n p)))))
+
+(define exp-ar-sequence
+  (lambda (a0 p)
+    (exp-sequence (lambda (n) (+ a0 (* p (- n 1)))))))
