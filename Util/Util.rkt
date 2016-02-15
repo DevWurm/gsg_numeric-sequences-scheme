8ee1161ebb9ac919740fc62994671f4c8392a66f
DevWurm <devwurm@gmx.net>
2016-02-15 09:07:50 +0100

    Adding foldl1, mimimum and maximum functions to the Util/Util module. foldl1 does an foldl on a list, starting with the first element of the list as accumulator. mimimum searches the smallest element in a list. maximum searchers the biggest element in a list.

--- Util/Util.rkt
+++ Util/Util.rkt
@@ -1,7 +1,22 @@
 #lang racket
 
 (provide sum)
+(provide foldl1)
+(provide minimum)
+(provide maximum)
 
 (define sum
   (lambda (ls)
     (foldl + 0 ls)))
+
+(define foldl1
+  (lambda (f ls)
+    (foldl f (car ls) (cdr ls))))
+
+(define minimum
+  (lambda (ls)
+    (foldl1 (lambda (acc n) (if (n < acc) n acc)) ls)))
+
+(define maximum
+  (lambda (ls)
+    (foldl1 (lambda (acc n) (if (n > acc) n acc)) ls)))
