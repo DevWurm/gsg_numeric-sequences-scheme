5e4f9fb9dc374e1e95f1f994a6d7f4246ed3e9b1
DevWurm <devwurm@gmx.net>
2016-02-22 18:38:09 +0100

    Adding stream-zip-with function to the Util/Util module. The function combines each element of a stream with the according element of another stream by using a given function.

--- Util/Util.rkt
+++ Util/Util.rkt
@@ -1,9 +1,13 @@
 #lang racket
 
+(require "../Streams/Streams.rkt")
+
 (provide sum)
 (provide foldl1)
 (provide minimum)
 (provide maximum)
+(provide stream-zip-with)
+(provide zip-with)
 
 (define sum
   (lambda (ls)
@@ -20,3 +24,9 @@
 (define maximum
   (lambda (ls)
     (foldl1 (lambda (acc n) (if (n > acc) n acc)) ls)))
+
+(define stream-zip-with
+  (lambda (f stm1 stm2)
+    (stream-cons
+     (f (stream-car stm1) (stream-car stm2))
+     (stream-zip-with f (stream-cdr stm1) (stream-cdr stm2)))))
